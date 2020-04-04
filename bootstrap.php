3f64596eeebb1ffc3da3ce47e11864706f1caf63
yhq1119 <33166425+yhq1119@users.noreply.github.com>
2020-04-05 03:51:41 +1000

    upload

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+require 'vendor/autoload.php';
+use Dotenv\Dotenv;
+
+use Src\System\DatabaseConnector;
+
+$dotenv = new DotEnv(__DIR__);
+$dotenv->load();
+
+$dbConnection = (new DatabaseConnector())->getConnection();
+
+
+?>
